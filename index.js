5d1ea8093dd54dadab6d8ef2e5d7cdba5014ba59
Grace Tian <gracetian4ever@gmail.com>
2020-03-11 16:05:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (let i = 0; i < musicians.length; i++) {
-      array.push(musicians[i] + " plays " +instruments[i]);
-    }
-  return array;
+function theBeatlesPlay(musicians,instruments) {
+  var x = [];
+  for (var i = 0; i < musicians.length; i++) {
+    x.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return x;
 }
 
 function johnLennonFacts (){

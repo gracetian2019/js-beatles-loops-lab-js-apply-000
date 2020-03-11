f49d818336e4df219220488b05c6322a2298697f
Grace Tian <gracetian4ever@gmail.com>
2020-03-11 15:52:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ function theBeatlesPlay(musicians, instruments) {
 
 theBeatlesPlay(musicians, instruments);
 
-var facts = [
+function johnLennonFacts (){
+   var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
-function johnLennonFacts (facts){
-    var newFacts=[];
+   var newFacts=[];
     var i=0;
     while(i<facts.length){
       newFacts.push(facts[i]+"!!!";)

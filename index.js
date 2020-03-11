b2e29edc6138ad4e4c0148c84657e3569cf73d7a
Grace Tian <gracetian4ever@gmail.com>
2020-03-11 15:21:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const musicians =["John Lennon","Paul McCartbey", "George Harrison", "Ringo Starr"]
-const instruments = [""]
+const musicians =["John Lennon","Paul McCartbey", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians,instruments){
   var array = [];
   
